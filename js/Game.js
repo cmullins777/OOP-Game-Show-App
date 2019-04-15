11f4f5ace88c5d8e91a2beccc91aa1563252917d
Cindy Mullins <mullins.cindy@gmail.com>
2019-04-15 15:13:52 -0700

    Step 7 - issue with game.activePhrase

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,44 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+  this.missed = 0;
+  this.phrases = this.createPhrases();
+  this.activePhrase = this.selectedPhrase;
+  }
+/**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in Game
+*/
+  createPhrases() {
+    const newPhrase =
+    [
+      new Phrase('All work and no play makes Jack a dull boy'),
+      new Phrase('Play it again Sam'),
+      new Phrase('There is no crying in baseball'),
+      new Phrase('If you build it he will come'),
+      new Phrase('Shake and bake'),
+    ];
+    return newPhrase;
+  }
+/**
+* Selects random phrase from phrases property
+* @return {Object} Phrase object chosen to be used
+*/
+
+  getRandomPhrase() {
+    let phrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    return phrase;
+    console.log(phrase);
+  }
+/**
+* Begins game by selecting a random phrase and displaying it to user
+*/
+
+  startGame() {
+    $("div[id='overlay']").hide();
+    let selectedPhrase = this.getRandomPhrase();
+  };
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+/**
+ * Display phrase on game board
+ **/
+  addPhraseToDisplay() {
+    const letters = [...this.phrase];
+    const displayPhrase = document.createElement('div');
+    displayPhrase.classList.add("section");
+    displayPhrase.setAttribute("id", "phrase");
+    const ul = document.createElement('ul');
+    displayPhrase.appendChild(ul);
+    console.log(letters);
+    letters.forEach(letter => {
+      const li = document.createElement('li');
+      if (letter >= 'a' && letter <= 'z') {
+        li.classList.add("hide", "letter", letter);
+        li.textContent = letter;
+        $("div[id='phrase'] ul").append(li);
+      } else {
+        li.classList.add("space");
+        li.textContent="";
+        $("div[id='phrase'] ul").append(li);
+      }
+    });
+  }
+}
